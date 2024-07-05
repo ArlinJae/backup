55d5545a61c4d0391c0c30220f6aff72ffbc39cd
Arlin Jae <jairelan.2005@gmail.com>
2024-07-05 15:43:49 -0700

    Main: Directories

--- install_all.sh
+++ install_all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 echo "[install.sh] XCode Command Line Tools"
-chmod +x /scripts/xcode_tools.sh
-./scripts/xcode_tools.sh
+chmod +x install_xcode_tools.sh
+./install_xcode.sh
 
 echo "[install.sh] Homebrew"
-chmod +x /scripts/brew_install.sh
-./scripts/brew_install.sh
+chmod +x install_brew.sh
+./install_brew.sh
+
+echo "[install.sh] Terminal"
+chmod +x install_terminal.sh
+./install_terminal.sh
+
+echo "[install.sh] All done!"
