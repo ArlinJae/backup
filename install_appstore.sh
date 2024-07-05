aa8679822bf6b3fd67158376c535aabd67317fc5
Arlin Jae <jairelan.2005@gmail.com>
2024-07-05 16:39:58 -0700

    AppStore: Created Script

---
+++ install_appstore.sh
@@ -0,0 +1,4 @@
+echo "[install_appstore.sh] Before this script proceeds, please ensure that you are signed in to the App Store. Press any key to continue..."
+read -n 1 -s -r -p ""
+
+echo "[install_appstore.sh] Installing App Store apps..."
