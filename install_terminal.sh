89926b77b352385af0381325f4bc73736ec07d30
Arlin Jae <jairelan.2005@gmail.com>
2024-07-08 18:00:32 -0700

    Terminal: Gitignore

--- install_terminal.sh
+++ install_terminal.sh
@@ -4,3 +4,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "[install_terminal.sh] Exporting ZSH config"
 cp .zshrc ~/.zshrc
 
+echo "[install_terminal.sh] Exporting global gitignore"
+cp .gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
